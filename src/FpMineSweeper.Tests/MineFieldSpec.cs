843bda12a0327ea76595bf3f5a73157fe5e6f189
심승용 <seungyongshim@nexon.co.kr>
2021-05-06 16:35:05 +0900

    테스트 추가

--- src/FpMineSweeper.Tests/MineFieldSpec.cs
+++ src/FpMineSweeper.Tests/MineFieldSpec.cs
@@ -14,9 +14,55 @@ namespace FpMineSweeper.Tests
                            .Cells
                            .Select(x => x.IsBomb)
                            .Should()
-                           .BeEquivalentTo(new[] {true, false, true,
-                                                   false, true, false,
-                                                   false, false, false});
+                           .BeEquivalentTo(new[] {true, false, false,
+                                                  false, true, false,
+                                                  true, false, false});
+        }
+
+        [Fact]
+        public void NearCountBombs()
+        {
+            mineField(3, 3).SetBombs(new[] { (0, 0), (1, 1), (0, 2) })
+                           .Cells
+                           .Select(x => x.NearBombsCount)
+                           .Should()
+                           .BeEquivalentTo(new[] { 1, 2, 1,
+                                                   3, 2, 1,
+                                                   1, 2, 1});
+        }
+
+        [Fact]
+        public void ConvertPosToIndex()
+        {
+            mineField(3, 3).ConvertPosToIndex((0, 0)).Should().Be(0);
+            mineField(3, 3).ConvertPosToIndex((1, 1)).Should().Be(4);
+            mineField(3, 3).ConvertPosToIndex((0, 2)).Should().Be(6);
+        }
+
+        [Fact]
+        public void GenerateBooleanMap()
+        {
+            mineField(3, 3).GenerateBooleanMap(2)
+                           .Should()
+                           .BeEquivalentTo(new[] { false, false, true,
+                                                   false, false, false,
+                                                   false, false, false });
+        }
+
+        [Fact]
+        public void CombineBooleanMap()
+        {
+            mineField(3, 3).CombineBooleanMap(new[] { true, false, false }, new[] { false, true, false })
+                           .Should()
+                           .BeEquivalentTo(new[] { true, true, false });
+        }
+
+        [Fact]
+        public void NearCells()
+        {
+            mineField(3, 3).NearCells((0, 0))
+                           .Should()
+                           .BeEquivalentTo(new[] { (1, 0), (0, 1), (1, 1) });
         }
     }
 }
--- src/FpMineSweeper/MineField.cs
+++ src/FpMineSweeper/MineField.cs
@@ -16,12 +16,51 @@ namespace FpMineSweeper
         public MineFieldReady SetBombs(IEnumerable<(int X, int Y)> bombsPosGenerator) =>
            new(this with
            {
-               Cells = bombsPosGenerator.Select(x => x.X + x.Y * Height)
-                                        .Select(x => from idx in Enumerable.Range(0, Width * Height)
-                                                     select idx == x ? true : false)
-                                        .Aggregate((x, y) => x.Zip(y, (a, b) => a || b))
-                                        .Select(x => cell(x))
+               Cells = bombsPosGenerator.Select(ConvertPosToIndex)
+                                        .Select(GenerateBooleanMap)
+                                        .Aggregate(CombineBooleanMap)
+                                        .Zip((from bombPos in bombsPosGenerator
+                                              from nearBombPos in NearCells(bombPos)
+                                              select nearBombPos)
+                                             .Select(ConvertPosToIndex)
+                                             .Select(x => from idx in Enumerable.Range(0, Width * Height)
+                                                          select idx == x ? 1 : 0)
+                                             .Aggregate((x, y) => x.Zip(y, (a, b) => a + b)),
+                                             (a, b) => cell(a, b))
            });
+
+        public int ConvertPosToIndex((int X, int Y) x) => x.X + x.Y * Width;
+
+        public IEnumerable<bool> CombineBooleanMap(IEnumerable<bool> x, IEnumerable<bool> y) =>
+            x.Zip(y, (a, b) => a || b);
+
+        public IEnumerable<bool> GenerateBooleanMap (int index) =>
+             from idx in Enumerable.Range(0, Width * Height)
+             select idx == index ? true : false;
+             
+
+        public IEnumerable<(int X, int Y)> NearCells((int X, int Y) pos) 
+        {
+            IEnumerable<(int X, int Y)> NearCellsInner()
+            {
+                yield return (pos.X - 1, pos.Y - 1);
+                yield return (pos.X, pos.Y - 1);
+                yield return (pos.X + 1, pos.Y - 1);
+                yield return (pos.X - 1, pos.Y);
+                yield return (pos.X + 1, pos.Y);
+                yield return (pos.X - 1, pos.Y + 1);
+                yield return (pos.X, pos.Y + 1);
+                yield return (pos.X + 1, pos.Y + 1);
+            }
+
+            bool CheckBoundary((int X, int Y) pos) => pos switch
+            {
+                (var x, var y) when x <0 || y <0 || x >= Width || y >= Height => false,
+                _ => true
+            };
+
+            return NearCellsInner().Where(CheckBoundary);
+        }
     }
 
     public record MineFieldReady : MineField
