c2ff678f54ef0241dae54106f8bc7d83f6184b48
심승용 <seungyongshim@nexon.co.kr>
2021-05-06 10:41:28 +0900

    테스

--- src/MineSweeper.Tests/MineFieldHelperSpec.cs
+++ src/MineSweeper.Tests/MineFieldHelperSpec.cs
@@ -19,12 +19,7 @@ namespace MineSweeper.Tests
         [Fact]
         public void NearCellGenerator()
         {
-            MineFieldHelper.NearCellGenerator(1, 1, x => new Cell(x.Item1, x.Item2))
-                           .Select(x => (x.X, x.Y))
-                           .Should()
-                           .BeEquivalentTo((0, 0), (1, 0), (2, 0),
-                                           (0, 1), (2, 1),
-                                           (0, 2), (1, 2), (2, 2));
+            false.Should().BeTrue();
         }
     }
 }
